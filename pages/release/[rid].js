770fa2edfa0089e8137e54af9e245d4770f8ed8b
Mark Weston <mweston@inviqa.com>
2022-11-01 11:41:46 +0000

    Add release details and basic layout to release page

--- pages/release/[rid].js
+++ pages/release/[rid].js
@@ -1,15 +1,24 @@
-import { useRouter } from 'next/router'
+import Link from "next/link";
 
 const Release = (release) => {
-  const router = useRouter()
-  const { rid } = router.query
-
-  console.log(release)
+  const releaseInfo = release.release.releases[0].basic_information;
 
   return (
     <div>
-      <p>Release: {rid}</p>
-      <p>Title {release.release.releases[0].basic_information.title}</p>
+      <div className="flex">
+        <div className="flex order-2 flex-col">
+          <p>{releaseInfo.title}</p>
+          <p>{releaseInfo.artists[0].name}</p>
+          <p>{releaseInfo.labels[0].name} | {releaseInfo.labels[0].catno}</p>
+          <p>{releaseInfo.year}</p>
+          <p>{releaseInfo.genres[0]}</p>
+          <p>{releaseInfo.formats[0].name}, {releaseInfo.formats[0].descriptions[0]}</p>
+        </div>
+        <div className="order-1">
+          <img src={release.release.releases[0].basic_information.cover_image} className="aspect-square" alt="Album artwork" />
+        </div>
+      </div>
+      <Link href={'/'} className="order-3">Back to the collection</Link>
     </div>
   )
 }
@@ -17,12 +26,13 @@ const Release = (release) => {
 export async function getServerSideProps(context) {
     // Call an Discogs API endpoint to get specific release
     const token = process.env.DISCOGS_API_TOKEN
-    const release_id = context.params.rid
-    const resUrl = `https://api.discogs.com/users/mweston/collection/releases/${release_id}?&token=${token}`;
+    const releaseId = context.params.rid
+    const resUrl = `https://api.discogs.com/users/mweston/collection/releases/${releaseId}?&token=${token}`;
     const res = await fetch(resUrl);
+    /**
+     * @todo Reduce the data returned to the individual release to reduce size and simplify outputting in component
+     */
     const release = await res.json();
-
-    console.log(resUrl);
   
     // Return release to Release component
     return {
